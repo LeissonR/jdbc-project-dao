fe65c245ace8741bff0aca30d0cebae38687949c
Leisson Rodrigues <leisson@gmail.com>
2023-05-01 21:45:29 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		Department dept = new Department(2, "MFG");
-		System.out.println(dept);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", sdf.parse("01/05/2023"), 3000.0, dept);
+
+		System.out.println(seller);
+
 	}
 
 }
